246988e100e3e4ab8080e2ec047b7c77b91ceb28
safari <email>
2019-05-13 16:50:56 +0000

    setup the database and made superuser

--- self_server/settings.py
+++ self_server/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'self_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nowaw$default',
+        'USER': 'nowaw',
+        'PASSWORD': 'food1231231',
+        'HOST': 'nowaw.mysql.pythonanywhere-services.com',
     }
 }
 
